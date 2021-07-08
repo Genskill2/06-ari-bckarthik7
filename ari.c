8930f1a2bc80a33d908887d3b28fe20f928b4393
bckarthik7 <44445298+bckarthik7@users.noreply.github.com>
2021-07-08 18:41:24 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,7 +1,7 @@
 string ari (string s){
   float a=0.0,b=0.0,c=0.0;
   float a;
-  int ar;
+  float ar;
   string s1;
   for(int i=0;s[i]!=\n;i++){
     if(s[i]==' ')
@@ -13,33 +13,33 @@ string ari (string s){
   a=4.71*(c/a)*(a/b);
   ar=ceil(a);
   switch (ar){
-    case 1 :  s1="Kindergarten"
+    case 1.0 :  s1="Kindergarten"
               break;
-    case 2 :  s1="First/Second Grade";
+    case 2.0 :  s1="First/Second Grade";
               break;
-    case 3 :  s1="Third Grade";
+    case 3.0 :  s1="Third Grade";
               break;
-    case 4 :  s1="Fourth Grade"
+    case 4.0 :  s1="Fourth Grade"
               break;
-    case 5 :  s1="Fifth Grade"
+    case 5.0 :  s1="Fifth Grade"
               break;
-    case 6 :  s1="Sixth Grade"
+    case 6.0 :  s1="Sixth Grade"
               break;
-    case 7 :  s1="Seventh Grade";
+    case 7.0 :  s1="Seventh Grade";
               break;
-    case 8 :  s1="Eighth Grade";
+    case 8.0 :  s1="Eighth Grade";
               break;
-    case 9 :  s1="Nineth Grade";
+    case 9.0 :  s1="Nineth Grade";
               break;
-    case 10 : s1="Tenth Grade";
+    case 10.0 : s1="Tenth Grade";
               break;
-    case 11 : s1="Eleventh Grade";
+    case 11.0 : s1="Eleventh Grade";
               break;
-    case 12 : s1="Twelfth grade";
+    case 12.0 : s1="Twelfth grade";
               break;
-    case 13 : s1="College student";
+    case 13.0 : s1="College student";
               break;
-    case 14 : s1="Professor";
+    case 14.0 : s1="Professor";
               break;
   }
   return s1;
